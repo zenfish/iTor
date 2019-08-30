91b6bf5f3f21b2be32750abb4d6bf8baa41ae205
zenfish <zenfish@gmail.com>
2019-08-29 19:38:32 -0700

    initial mostly untested salvo

---
+++ README.md
@@ -0,0 +1,126 @@
+A script - FOR MACs ONLY - that fires up Tor in a docker container, deals with details
+-----
+
+DO NOT RUN, lol. Still testing it out.
+
+MAC ONLY!
+
+This grabs a Tor docker container - the nifty little one at:
+
+    https://github.com/rdsubhas/docker-tor-privoxy-alpine
+
+... and tries to set up all the ipctl, proxy, etc. stuff.  It tests itself
+as it goes along, eventually trying to connect to check.torproject.org
+to see if they think it's Tor or not.
+
+
+IN THEORY. 
+
+After running this your browser will use the Tor container as a socks
+proxy - typically set by menus/by hand in:
+
+    System Preferences->HW-interface->Advanced->Proxies->SOCKS Proxy
+
+It'll create a pfctl configuration file to shunt traffic from local ports
+to the docker proxy. Eventually this could take over 443, 80, etc., but
+I was too chicken initially to do this. However, you could check that it's
+working with curl like thus (this is done in the script, but...)
+
+    curl --socks5 http://localhost:9050 https://check.torproject.org/|grep Congratulations
+
+
+I'm sure it'll keel over in all sorts of ways.
+
+
+TODO:
+
+    - lots
+
+    - properly deal with CIDRs that Tor shouldn't be seeing (e.g. 10/8, etc.)
+
+
+
+
+Output
+-----
+
+Here's what the script looks like when I run it -
+
+    $ ./iTor.sh
+
+    trying to start the Tor Docker container...
+    Unable to find image 'rdsubhas/tor-privoxy-alpine:latest' locally
+    latest: Pulling from rdsubhas/tor-privoxy-alpine
+    ff3a5c916c92: Pulling fs layer
+    3db7ce00a871: Pulling fs layer
+    3570578356bf: Pulling fs layer
+    3570578356bf: Verifying Checksum
+    3570578356bf: Download complete
+    ff3a5c916c92: Verifying Checksum
+    ff3a5c916c92: Download complete
+    ff3a5c916c92: Pull complete
+    3db7ce00a871: Verifying Checksum
+    3db7ce00a871: Download complete
+    3db7ce00a871: Pull complete
+    3570578356bf: Pull complete
+    Digest: sha256:1f48b2d88a0f44fdd7e79fe60abb8c15f6af0bf716e13243c44c1598c31ebc7b
+    Status: Downloaded newer image for rdsubhas/tor-privoxy-alpine:latest
+    Using interface en0, loopback lo0, IP/CIDR: 192.168.0.132/24
+    
+    The pfctl configuration file (torry-pfctl.conf) has been successfully created...
+    The pfctl command must be run as root... will prompt for sudo....
+    
+    Mercelessly nuking all pfctl rules, then trying to put in Tor ones
+    
+    Password:
+    
+    ... if the gods are feeling benign... traffic sent to port localhost:5080 (web) or localhost:5080   should go through tor... trying to test -
+    
+    
+    
+    
+    congrats, looks good...!  https://check.torproject.org/ should corraborate
+    
+    
+    
+    
+    FINALLY - setting sox proxy for BROWSERs in the System Settings
+    
+    (networksetup -setsocksfirewallproxy Ethernet localhost )
+    
+    To stop using Tor et al, stop the running docker container:
+    
+    	docker stop 8a2f878a5b4cd7aa6cf6757609d69410b23eaab69eade6b95729515299da4876
+    
+    Clear the pfctl rules with:
+    
+    	sudo pfctl -d && sudo pfctl -e
+    
+    Remove the old pfctl configuration file
+    
+    	rm torry-pfctl.conf
+    
+    And finally kill off the socks network System Preferences
+    
+    	networksetup -setsocksfirewallproxystate Ethernet off
+    
+    good luck!
+
+
+
+
+
+Details
+-----
+
+To get the system back to normal, you need to kill off the container, clear pfctl, 
+and set back the networksetup proxy state. The script tells you all this, but basically
+it's something like:
+
+    	docker stop $dockerID
+    
+    	sudo pfctl -d && sudo pfctl -e
+    
+    	networksetup -setsocksfirewallproxystate Ethernet off
+    
+
---
+++ iTor.sh
@@ -0,0 +1,209 @@
+
+#
+# fire up tor docker container, create an ipctl conf file for docker tor stuff, and rock 'n' roll
+#
+
+# container name
+dtor_name="rdsubhas/tor-privoxy-alpine"
+
+# web proxy = 8118
+# sox = 9050
+dtor_web_outside="8118"
+dtor_web_inside="8118"
+dtor_sox_outside="9050"
+dtor_sox_inside="9050"
+
+pfctl_to_docker="5080"
+tor_server=localhost    # where docker opens the tor port
+
+
+# is it already running?
+# docker ps -a
+# CONTAINER ID        IMAGE               COMMAND                  CREATED              STATUS              PORTS                                            NAMES
+# 807f68d6c687        zdock-tor           "tini"                   4 seconds ago        Up 2 seconds        0.0.0.0:8118->8118/tcp, 0.0.0.0:9050->9050/tcp   vigorous_leavitt
+
+did=$(docker ps -a | awk '$2~"'"$dtor_name"'" && / Up/ { print $1; exit }')
+
+# drez=$(docker inspect -f {{.State.Running}} "$dtor_name" true)
+
+if [ ! -z "$did" ]; then
+    echo "Container is already running!  Bailing out...."
+    exit 7
+fi
+
+# die die die on errz... have to do it after the above! :)
+set -e
+
+echo trying to start the Tor Docker container...
+
+did=$(docker run -d -p $dtor_web_outside:$dtor_web_outside -p $dtor_sox_outside:$dtor_sox_inside "$dtor_name")
+
+if ! docker top $did &> /dev/null; then
+    echo "Error starting the Tor Docker container"
+    exit 6
+fi
+
+#
+# save some typing
+#
+function errz () {
+
+    problem=$1
+    var=$1
+
+    if [ -z "$var" ] ; then
+        echo "$problem needs to be set"
+        exit 3
+    fi
+}
+
+#
+# Next: figure out the IP addr, interface, CIDR block, and loopback...
+#
+
+#
+# try to determine who talks to the 'net
+#
+# if this doesn't work you can manually set (e.g. "INTERFACE=en0" or whatever)
+#
+INTERFACE=$(netstat -rn|awk '/default/ && ! /::/ { print $NF; exit}')
+
+errz "The interface to the internet" $INTERFACE
+
+# find the HW interface name associated with the interface we're using
+HW=$(networksetup -listallhardwareports | awk '/Port/ { split($0, p, ":"); hw = p[2] } /Device/ { if ($2 == "'"$INTERFACE"'") print hw }' | sed 's/^ *//')
+
+errz "Hardware interface name" $HW
+
+LOOPY=$(ifconfig | awk '/LOOPBACK/ { print $1; exit}' | sed 's/://')
+
+errz "Loopback interface" $LOOPY
+
+# time to jump hoops for CIDR
+# print first one if lots of ints go whereever... ignore ipv6 for now
+IP=$(ifconfig $INTERFACE | awk '/inet / { print $2}')
+
+errz "IP Address" $IP
+
+#
+# perl magic via https://stackoverflow.com/questions/47746535/bash-how-do-i-convert-a-hex-subnet-mask-into-bit-form-or-the-dot-decimal-addres
+#
+CIDR=$(ifconfig en0 | awk '/inet / { printf $4 }' | sed 's/0x//' | perl -pe '$_ = unpack("B32", pack("H*", $_)); s/0+$//g; $_ = length')
+
+errz "The CIDR block" $CIDR
+
+echo Using interface $INTERFACE, loopback $LOOPY, IP/CIDR: $IP/$CIDR
+
+echo ""
+
+#
+# now create the Tor conf file with the above intel
+#
+
+PFCTL="torry-pfctl.conf"
+
+# don't overwrite out of courtesy....
+if [ -s "$PFCTL" ] ; then
+    echo "Cowardly bailin' to prevent overwriting the Tor configuration file ($PFCTL)"
+    exit 4
+fi
+
+cat > "$PFCTL" <<_EOT_
+
+#
+# to test syntax
+#
+#   pfctl -n -v -f $PFCTL
+#
+
+# run, debug+verbose+more debug
+#
+#   pfctl -g -x loud -v -f $PFCTL
+#
+
+#
+# show all rules, etc.
+#
+#       pfctl -s all
+#
+
+# mac can't use docker's host, so have to work with ports
+
+rdr pass on lo0 inet proto tcp from any to any port $pfctl_to_docker -> $tor_server port $dtor_web_outside
+
+_EOT_
+
+# rdr pass on lo0 inet proto tcp from any to any port $proxy_port -> $tor_server port $tor_port
+# proxy_port = $pfctl_to_docker
+# tor_port   = $dtor_web_outside
+# tor_server = localhost
+
+#
+# finis
+#
+
+if [ $? ]; then
+    echo "The pfctl configuration file ($PFCTL) has been successfully created..."
+else
+    echo "Error (code: $?) creating the pfctl configuration file ($PFCTL)... bailin' out..."
+    exit 5
+fi
+
+if [ $(id -u) ]; then
+    echo "The pfctl command must be run as root... will prompt for sudo...."
+fi
+
+# .... sigh...
+set +e
+
+# clear the decks
+echo ""
+echo Mercelessly nuking all pfctl rules, then trying to put in Tor ones
+echo ""
+sudo pfctl -d &> /dev/null
+sudo pfctl -e &> /dev/null
+cat "$PFCTL" | sudo pfctl -e -f - &> /dev/null
+
+echo ""
+echo "... if the gods are feeling benign... traffic sent to port $tor_server:$pfctl_to_docker (web) or $tor_server:$pfctl_to_docker   should go through tor... trying to test -"
+echo ""
+
+curl --proxy http://$tor_server:$pfctl_to_docker https://check.torproject.org/ 2> /dev/null | grep -q Congratulations
+
+if [ $? ]; then
+    echo -e "\n\n"
+    echo "congrats, looks good...!  https://check.torproject.org/ should corraborate"
+    echo -e "\n\n"
+else
+    echo "buzz... after all this, you'd think it'd work, eh?  Sorry...."
+fi
+
+
+echo
+echo FINALLY - setting sox proxy for BROWSERs in the System Settings
+echo
+echo "(networksetup -setsocksfirewallproxy $HW localhost $PORT)"
+echo
+networksetup -setsocksfirewallproxy "$HW" localhost "$dtor_sox_outside"
+
+echo To stop using Tor et al, stop the running docker container:
+echo 
+echo -e "\tdocker stop $did"
+echo 
+echo Clear the pfctl rules with:
+echo
+echo -e "\tsudo pfctl -d && sudo pfctl -e"
+echo
+echo Remove the old pfctl configuration file
+echo
+echo -e "\trm $PFCTL"
+echo
+echo And finally kill off the socks network System Preferences
+echo
+echo -e "\tnetworksetup -setsocksfirewallproxystate $HW off"
+echo
+echo "good luck!"
+echo
+
+
+
